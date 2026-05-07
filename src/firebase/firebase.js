3d1b47576aa1217cfb76b07bf5e71cdd310e5a59
4ahd3ssam <shahd.edakrory@gmail.com>
2026-05-07 23:54:13 +0300

    Import auth package

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyCfrTOPzSuv_xwKnvXmSgQz0VokPTLmbaw",
   authDomain: "zenfit-6410a.firebaseapp.com",
@@ -16,6 +13,7 @@ const firebaseConfig = {
   measurementId: "G-6K8J9B09EP"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
