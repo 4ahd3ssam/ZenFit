93a370072be6d7f2672d05a440bcbcdfeebbc2dd
4ahd3ssam <shahd.edakrory@gmail.com>
2026-05-08 22:50:16 +0300

    Center the content

--- src/components/layout/PageLayout.jsx
+++ src/components/layout/PageLayout.jsx
@@ -1,6 +1,6 @@
 export const PageLayout = ({ children }) => {
     return (
-        <main className="pt-20 pb-8 bg-gray-50 dark:bg-zinc-950 min-h-screen transition-all duration-200">
+        <main className="pt-20 pb-8 bg-gray-50 dark:bg-zinc-950 min-h-screen transition-all duration-200 flex items-center justify-center">
             <div className="container mx-auto px-2 sm:px-4">
                 {children}
             </div>
