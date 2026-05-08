7e0e9559873bbcf15c176c4c968826baadd51fd7
4ahd3ssam <shahd.edakrory@gmail.com>
2026-05-08 17:03:21 +0300

    Update fav icon size to be dynamic

--- src/components/common/FavIcon.jsx
+++ src/components/common/FavIcon.jsx
@@ -1,15 +1,15 @@
 import { Flame } from "lucide-react"
 
-export const FavIcon = ({ containerSize = 10, iconSize = 5 }) => {
+export const FavIcon = ({ containerSize = 10, iconSize = 25 }) => {
     return (
         <div className={`
                     w-${containerSize} h-${containerSize} me-3 
                     bg-gradient-to-br from-orange-400 to-orange-600 
                     dark:from-orange-500 dark:to-orange-700
                     border border-orange-300/40 dark:border-orange-400/30 
-                    rounded-full flex items-center justify-center shadow-md
+                    rounded-2xl flex items-center justify-center shadow-md
                 `}>
-            <Flame className={`text-white dark:text-zinc-900 w-${iconSize} h-${iconSize} drop-shadow-sm`} />
+            <Flame size={iconSize} className={`text-white dark:text-zinc-900 drop-shadow-sm`} />
         </div>
     )
 
