8c9301f0d53da0b576160067f9cfd51e255d39fd
4ahd3ssam <shahd.edakrory@gmail.com>
2026-05-10 23:40:43 +0300

    complete protected route component for authentication

--- src/features/auth/ProtectedRoute.jsx
+++ src/features/auth/ProtectedRoute.jsx
@@ -1,3 +1,13 @@
+import { useAuthStore } from "../../store/authStore";
+import { Navigate } from "react-router-dom";
+
 export const ProtectedRoute = ({ children }) => {
-    return children;
+    const { user } = useAuthStore()
+
+    if (!user) {
+        return <Navigate to="/login" replace />
+    }
+    else {
+        return children
+    }
 }
